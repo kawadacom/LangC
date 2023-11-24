20c3251cf0f7c8ac757abb128a8ef0bc8341248d
kawadacom <147793064+kawadacom@users.noreply.github.com>
2023-11-24 13:49:39 +0900

    Add files via upload

--- while_study.c
+++ while_study.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
-    int i = 1;
-    while(i <= 10){
-        printf("%d\n",i);
+    double i = -1;
+    while(i <= 10.0){
+        printf("%f\n",i);
         i++;
     }
     return 0;
