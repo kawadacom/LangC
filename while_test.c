e5479b297f7ef9661c91b0e9fd4a9a9f5d4ee9d3
kawadacom <147793064+kawadacom@users.noreply.github.com>
2023-11-24 13:53:10 +0900

    Add files via upload

--- while_test.c
+++ while_test.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
     int count = 1;
-    while(count <= 10){
-        printf("%d\n",count);
+    while(count * 2 <= 10){
+        printf("%d\n",count*2);
         count++;
     }
     return 0;
